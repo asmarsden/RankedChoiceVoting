b47fdaa1732dfbfa0d933c588fc22f4f2378a203
griff <griff@DESKTOP-P39MNAI>
2021-09-23 18:36:58 -0500

    Initial commit of Hello World Application

---
+++ RankChoiceVoting/src/main/java/com/CS495/RankChoiceVoting/RankChoiceVotingApplication.java
@@ -0,0 +1,13 @@
+package com.CS495.RankChoiceVoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankChoiceVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankChoiceVotingApplication.class, args);
+	}
+
+}
