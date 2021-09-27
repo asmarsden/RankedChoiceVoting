d037f6389538b3b0dc928e06a710b068e078f656
griff <griff@DESKTOP-P39MNAI>
2021-09-27 18:38:32 -0500

    Added sample POJO class and repository for use with Mongo; update app properties to connect to our hosted mongoDB

--- RankChoiceVoting/src/main/java/com/CS495/RankChoiceVoting/RankChoiceVotingApplication.java
+++ RankChoiceVoting/src/main/java/com/CS495/RankChoiceVoting/RankChoiceVotingApplication.java
@@ -1,13 +1,53 @@
 package com.CS495.RankChoiceVoting;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.CS495.RankChoiceVoting.Model.Customer;
+import com.CS495.RankChoiceVoting.Repository.CustomerRepository;
 
 @SpringBootApplication
-public class RankChoiceVotingApplication {
+@EnableMongoRepositories
+public class RankChoiceVotingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RankChoiceVotingApplication.class, args);
-	}
+  @Autowired
+  private CustomerRepository repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(RankChoiceVotingApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    repository.deleteAll();
+
+    // save a couple of customers
+    repository.save(new Customer("Alice", "Smith"));
+    repository.save(new Customer("Bob", "Smith"));
+
+    // fetch all customers
+    System.out.println("Customers found with findAll():");
+    System.out.println("-------------------------------");
+    for (Customer customer : repository.findAll()) {
+      System.out.println(customer);
+    }
+    System.out.println();
+
+    // fetch an individual customer
+    System.out.println("Customer found with findByFirstName('Alice'):");
+    System.out.println("--------------------------------");
+    System.out.println(repository.findByFirstName("Alice"));
+
+    System.out.println("Customers found with findByLastName('Smith'):");
+    System.out.println("--------------------------------");
+    for (Customer customer : repository.findByLastName("Smith")) {
+      System.out.println(customer);
+    }
+
+  }
 
 }
---
+++ RankChoiceVoting/src/main/java/com/CS495/RankChoiceVoting/Repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.CS495.RankChoiceVoting.Repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.CS495.RankChoiceVoting.Model.Customer;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+  public Customer findByFirstName(String firstName);
+  public List<Customer> findByLastName(String lastName);
+
+}
