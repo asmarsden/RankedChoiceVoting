5d55a2788b362ccd213a3994332f2001981d0680
Aislinn Marsden <asmarsden@crimson.ua.edu>
2021-11-15 15:49:37 -0600

    updated tests, not finished yet

--- src/test/java/com/CS495/RankChoiceVoting/RankChoiceVotingApplicationTests.java
+++ src/test/java/com/CS495/RankChoiceVoting/RankChoiceVotingApplicationTests.java
@@ -29,128 +29,196 @@ class RankChoiceVotingApplicationTests {
 	@Autowired
 	private Poll poll;
 	private String code;
-	PollDTO dto;
+	PollDTO pollDTO;
+	private PollRepository repository;
+	private PollMapper pollMapper;
+	private PollService pollService;
+	private BallotService balService;
+	private BallotMapper balMapper;
 	
 	@Test
 	void contextLoads() {
 	}
 	
 	@Test
+	@DisplayName("Testing poll creation")
 	void createPollTest() throws Exception {
 		String[] bal = {"Wendy's", "Arby's", "McDonald's", "Burger King", "KFC", "Taco Bell"};
 		poll.setCreatorId("asmarsden");
 		poll.setQuestion("Where should we get dinner?");
 		poll.setCandidates(bal);
-		dto.polltoDTO(poll); //hmm its having trouble finding this for some reason
-		dto.createPoll(dto);
+		pollDTO = pollMapper.polltoDTO(poll); 
+		pollService.createPoll(pollDTO);
 		//does this return the poll code? i have it set so it makes the poll code here but. hm
-		code = dto.getUrlCode();
-		
+		code = poll.getUrlCode();
+		//last thing i need to figure out is what exactly i should compare for testing, gonna look at examples later
 
 	}
 	
 	@Test
+	@DisplayName("Testing poll retrieval by code")
 	void getPollCode() throws Exception {
-		//i can't really write this until i know how the ballot is going to look data-wise
-		dto.getPollByCode(code);
+		pollService.findPollByUrlCode(code);
+		
 	}
 	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest1() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test1");
 		int[] ranks = {1, 2, 3};
 		bal.setRanking(ranks);
-		BallotDTO balDTO = new BallotDTO();
-		balDTO.ballottoDTO(bal);
-		//still dont know if im doing this right
-		dto.updatePoll();//something something ill fix this later
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
+		
 	}
 	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest2() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test2");
 		int[] ranks = {1, 2, 3};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest3() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test3");
 		int[] ranks = {3, 2, 1};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest4() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test4");
 		int[] ranks = {4, 3, 2};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest5() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test5");
 		int[] ranks = {4, 1};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest6() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test6");
 		int[] ranks = {1, 4, 5, 3, 2};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest7() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test7");
 		int[] ranks = {1, 2, 5};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest8() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test8");
 		int[] ranks = {3, 4};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest9() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test9");
 		int[] ranks = {1};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing vote functionality and populating test poll for later calculations")
 	void voteTest10() throws Exception {
 		Ballot bal = new Ballot();
 		bal.setParentPollCode(code);
 		bal.setName("test10");
 		int[] ranks = {4, 5, 3};
 		bal.setRanking(ranks);
+		BallotDTO balDTO = balMapper.ballottoDTO(bal);
+		balService.castBallot(balDTO, code);
+		String balcode = balDTO.getBallotCode();
+		poll.appendBallot(balcode);
 	}
+	
 	@Test
+	@DisplayName("Testing functionality of closing poll")
 	void closePollTest() throws Exception {
 		//i dont see a function for this? i guess thatll be added with the calculate results stuff
 	}
+	
 	@Test
+	@DisplayName("Testing result calculation functionality")
 	void calculateVoteResultsTest() throws Exception {
 		//this function has not been finished
 	}
+	
 	@Test
+	@DisplayName("Testing poll deletion functionality")
 	void deletePollTest() throws Exception {
-		deletePoll(dto); 
+		pollService.deletePoll(pollDTO); 
 	}
 	
 }
